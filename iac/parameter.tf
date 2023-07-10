bd3c0b537ab57065a83ce3ab6afe4af926c4e3bc
Sake Pon <zzymukerzz@gmail.com>
2023-07-10 20:43:57 +0900

    ignore_change ssm value

--- iac/parameter.tf
+++ iac/parameter.tf
@@ -2,16 +2,31 @@ resource "aws_ssm_parameter" "slack_signing_secret" {
   name  = "slack_signing_secret"
   type  = "String"
   value = "dummy"
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
 
 resource "aws_ssm_parameter" "slack_app_token" {
   name  = "slack_app_token"
   type  = "String"
   value = "dummy"
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
 
 resource "aws_ssm_parameter" "slack_bot_token" {
   name  = "slack_bot_token"
   type  = "String"
   value = "dummy"
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
